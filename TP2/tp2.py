efbe49982689c9b6aa74baeb6e048589ab09e706
muribe <muribe@fi.uba.ar>
2024-10-06 00:54:32 -0300

    TP2 solucion correcta

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -1,54 +1,62 @@
-from os import utime
+# Maximizar la ganancia consiste en agregar o no un elemento i.
+# Si no se agrega, la mejor solucion es aquella que no lo tiene.
+# Si se agrega, la mejor solucion es aquella que lo tiene, pero tiene W-Pi peso.
 
+# Maximizar la ganancia de Sophi consiste en agregar o no un elemento del fin/inicio
 
-def tp2(monedas):
+from math import ceil
+
+def reconstruirSolucion(OPT):
+    n = len(OPT)
+    f = 0
+    c = n - 1
+    solucion = []
+    turnoSophia = True
+    while len(solucion) < ceil(n / 2):
+        if not turnoSophia: # Si es el turno de Mateo
+            if OPT[f][c] == monedas[c] - OPT[f][c-1]: # Si Mateo eligio el elemento del final
+                c -= 1
+            elif OPT[f][c] == monedas[f] - OPT[f+1][c]: # Si Mateo elegio el elemento del principio
+                f += 1
+
+        elif OPT[f][c] == monedas[c] - OPT[f][c-1]:  # Si Sophia eligio el elemento del final
+            solucion.append(monedas[c])
+            c -= 1
+        elif OPT[f][c] == monedas[f] - OPT[f+1][c]:  # Si Sophia eligio el elemento del comienzo
+            solucion.append(monedas[f])
+            f += 1
+
+        turnoSophia = not turnoSophia
+    return solucion
+
+def juego(monedas):
     n = len(monedas)
-    if n % 2 == 0:
-        k = n // 2
-    else:
-        k = (n // 2) + 1
+    OPT = [[0 for _ in range(n)] for _ in range(n)]
 
-    OPT = [0 for _ in range(k)]
+    for j in range(n):
+        for i in range(j, -1, -1):
+            largo = abs(i - j) + 1
 
-    izq = 0
-    der = n - 1
-
-    for i in range(k):
-        primero = monedas[izq]
-        ultimo = monedas[der]
-        segundo = 0
-        anteultimo = 0
-
-        if izq + 1 < der:
-            segundo = monedas[izq + 1]
-            diferencia_por_primero = primero - max(segundo, ultimo)
-        else:
-            diferencia_por_primero = primero
-        if der - 1 > izq:
-            anteultimo = monedas[der - 1]
-            diferencia_por_ultimo = ultimo - max(primero, anteultimo)
-        else:
-            diferencia_por_ultimo = ultimo
-
-        if diferencia_por_primero >= diferencia_por_ultimo:
-            OPT[i] = primero
-            if segundo >= ultimo:
-                der -= 2
+            if largo == 1:
+                # Caso base
+                OPT[i][j] = monedas[i]
+            elif largo == 2:
+                # Caso base , dos monedas Sofia toma la mejor
+                OPT[i][j] = abs(monedas[i] - monedas[j])
             else:
-                der -= 1
-                izq += 1
-        else:
-            if primero >= anteultimo:
-                der -= 1
-                izq += 1
-            else:
-                der -= 2
-            OPT[i] = ultimo
+                # Si toma la primer moneda...
+                if i == n - 1:
+                    # Si toma la primer moneda, Mateo toma la mejor de las que quedan
+                    OPT[i][j] = monedas[i] - OPT[i+1][j]
+                # Si toma la ultima moneda...
+                else:
+                    # Si toma la ultima moneda, Mateo toma la mejor de las que quedan
+                    OPT[i][j] = max(monedas[i] - OPT[i+1][j], monedas[j] - OPT[i][j-1])
 
-    return OPT
 
-l1 = [5, 2, 1, 5, 7, 6, 4]
-print(tp2(l1))
+    return reconstruirSolucion(OPT)
 
-l2 = [3, 5, 10 ,7]
-print(tp2(l2))
+
+monedas = [5, 2, 1, 4, 7, 6]
+resultado = juego(monedas)
+print(resultado)
